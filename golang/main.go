932ee8772163b78394c7f554fe2d7e8e0560bf05
xakristl8 <xakristl8@bk.ru>
2024-09-30 19:44:07 +0300

    лаба4: lab4: начало роботы над 4 лабой

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"isuct.ru/informatics2022/laba4"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	var y float64
+
+	var a float64 = 2.5
+	var b float64 = 4.6
+	var x [5]float64 = [5]float64{1.2, 1.36, 1.57, 1.93, 2.25}
+
+	for i := 0; i < len(x); i++ {
+		y = laba4.Colcul(a, b, x[i])
+		fmt.Println("при x =", x[i], " y =", y)
+	}
 }
