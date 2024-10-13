95eec3e8e2267dd8412b0046e062a89c332886f6
xakristl8 <xakristl8@bk.ru>
2024-10-13 20:26:29 +0300

    начло работы над теорией управления

---
+++ golang/funcTy/funcTy.go
@@ -0,0 +1,19 @@
+package funcTy
+
+func Ak() float64 {
+	var ak float64 = 0
+	// for i := 1; i < m; i++ {
+
+	// }
+	return ak
+}
+
+func W() float64 {
+	var W float64 = 0
+	return W
+}
+
+func R() float64 {
+	var r float64 = 0
+	return r
+}
