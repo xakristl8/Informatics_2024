878022eab45e472036e7283a8d44258b38b61ac2
xakristl8 <xakristl8@bk.ru>
2024-10-14 06:30:46 +0300

    изучение структур

--- golang/main.go
+++ golang/main.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	answer := 42
-	fmt.Println(&answer)
+	var cookiesShop Shop = Shop{100, 5, 0}
+	var queue []Queue = []Queue{{"олег", 0, 10}, {"вова", 0, 15}, {"акула", 0, 0}}
+	queue[1] = queue[1].SetHaveBuy(1)
+
+	queue = queue[1:]
+
+	fmt.Println(queue, cookiesShop)
+}
+
+type Shop struct {
+	QuantitySale int
+	Price        int
+	Money        int
+}
+
+type Queue struct {
+	Name    string
+	Money   int
+	HaveBuy int
+}
+
+func (q Queue) SetHaveBuy(i int) Queue {
+	q.HaveBuy = i
+	return q
 }
