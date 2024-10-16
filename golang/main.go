85e51b3a20849e07d036d5a90ae4799542a3f2a5
xakristl8 <xakristl8@bk.ru>
2024-10-16 17:51:05 +0300

    начало изучение вложеных структур и интерфейсы

--- golang/main.go
+++ golang/main.go
@@ -1,39 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
+type Animal struct {
+	name string
+	age  int
+}
+
+type Cat struct {
+	animalInfo Animal
+	color      string
+}
 
 func main() {
-	var cookiesShop Shop = Shop{100, 5, 0}
-	var queue []Buyer = []Buyer{{"Олег", 100}, {"Вова", 0}, {"акула", 1000}}
-	var Buyers []Buyer = []Buyer{}
-
-	queue[0], cookiesShop = queue[0].Buy(cookiesShop)
-
-	Buyers = append(Buyers, queue[0])
-	queue = queue[1:]
-
-	fmt.Println("очередь:", queue)
-	fmt.Println("покупатели:", Buyers)
-	fmt.Println("магазин печенек:", cookiesShop)
-}
-
-type Shop struct {
-	quantitySale int
-	price        int
-	money        int
-}
-
-type Buyer struct {
-	name  string
-	money int
-}
-
-func (b Buyer) Buy(s Shop) (Buyer, Shop) {
-	if b.money > s.price {
-		b.money -= s.price
-		s.quantitySale -= 1
-	}
-	return b, s
 }
